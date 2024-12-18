1cbeb2ab92aef34b4fbbf36629250dc9ea4a10fe
hemantsingh555 <9696888046hemant@gmail.com>
2024-12-18 13:48:10 +0530

    Add files via upload

---
+++ 2darray.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	
+    int arr[3][2] = { { 0, 1 }, { 2, 3 }, { 4, 5 } };
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 2; j++) {
+            printf("arr[%d][%d]: %d    ", i, j, arr[i][j]);
+        }
+          printf("\n");
+    }
+    return 0;
+}
